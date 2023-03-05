a74aaefafb6f86b1df9445e73e1abf13e0e1413d
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-03-05 14:18:19 +0600

    create a server

---
+++ http.js
@@ -0,0 +1,11 @@
+// Creating raw http model
+
+/* 
+    http module helps to work with network functionalities.
+
+*/
+
+const http = require("http");
+
+// create server
+const server = http.createServer();
