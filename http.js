8c451d3ee0eab8a9d11c68f773e6e96fc70f89a0
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-03-05 14:29:35 +0600

    handle request and response using http module

--- http.js
+++ http.js
@@ -8,4 +8,17 @@
 const http = require("http");
 
 // create server
-const server = http.createServer();
+/* 
+    create server recieves a callback where request and response can be handled
+*/
+const server = http.createServer((req, res) => {
+  res.write("Hello World!");
+  res.end();
+});
+
+/* 
+    Server has some default methods to work with.. Such as server.listen. Listen method creates an event to always hear the server with the help of event loop. 
+*/
+
+server.listen(3000); //here we can define port for the sever to run...WE can create as many as server within an application
+console.log("Listening on port 3000");
